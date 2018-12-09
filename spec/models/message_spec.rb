8ab05f678f26d89686a9b91519c206a2f0c569c5
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2018-12-09 18:57:06 +0900

    テスト導入の続き

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     context 'can save' do
-      it 'is valid with content' do
+      it 'is valid with text' do
         expect(build(:message, image: nil)).to be_valid
       end
 
@@ -12,25 +12,27 @@ RSpec.describe Message, type: :model do
       end
 
       it 'is valid with text and image' do
-        expect(build(:message, text: nil)).to be_valid
-      end
-
-      it 'isvalid with text and image' do
         expect(build(:message)).to be_valid
       end
     end
 
     context 'can not save' do
-      it 'is invalid without content and image' do
+      it 'is invalid without text and image' do
         message = build(:message, text: nil, image: nil)
         message.valid?
-        expect(message.errors[:text]).to include('を入力してください')
+        expect(message.errors[:text]).to include
       end
 
-      it 'is invalid without user_id' do
+      it 'is invalid without group_id' do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include
+      end
+
+      it 'is invaid without user_id' do
         message = build(:message, user_id: nil)
         message.valid?
-        expect(message.errors[:user]).to include('を入力してください')
+        expect(message.errors[:user]).to include
       end
     end
   end
