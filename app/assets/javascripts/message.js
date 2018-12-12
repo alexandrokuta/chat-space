e875b6f3d68873a9e2c2f2d06522aa60227ae557
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2018-12-12 14:45:55 +0900

    function ajax end

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,35 +1,38 @@
 $(function(){
-  function buildHTML(message) {
-    var html =`<ul>
-                 <li class="message__name">
-                   ${message.name}
-                 </li>
-                 <li class="message__time">
-                   ${message.created_at}
-                 </li>
-                 <li class="message__text">
-                  ${message.text}
-                 </li>
-              </ul>`
+  function buildHTML(message){
+    var html = `<ul>
+            <li class="message__name">
+              ${message.name}
+            </li>
+            <li class="message__time">
+              ${message.created_at}
+            </li>
+            <li class="message__text">
+             ${message.text}
+            </li>
+            </ul>`
     return html;
-  }
+}
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
-    console.log(url)
+    var href = window.location.href + '/messages'
     $.ajax({
-      url: url,
+      url: href,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
       contentType: false
-    })
-    .done(function(data) {
+    });
+    .done(function(data){
       var html = buildHTML(data);
-      $('.message__box').append(html)
+      $('.message__box').append(html);
       $('.form_field').val('')
-    })
-  })
+    });
+    .fail(function(){
+      alert('error');
+    });
+  });
 });
+
